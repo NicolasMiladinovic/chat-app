d8233d21f24c71b8724d214b0a43958733cedb51
NicolasMiladinovic <nicolas.miladinovic@gmail.com>
2022-09-27 23:58:51 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const { ApolloServer } = require('apollo-server');
+
+const { sequelize } = require('./models/index.js')
+
+const resolvers = require('./graphql/resolver.js')
+const typeDefs = require('./graphql/typeDefs.js')
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+})
+
+server.listen().then(({ url }) => {
+    console.log(`🚀 Server ready at ${url}`)
+
+    sequelize
+        .authenticate()
+        .then(() => console.log('Database connected'))
+        .catch((e) => console.log(e))
+})
