efc11893c3def4f526a4473fd9500570dbcfc814
NicolasMiladinovic <nicolas.miladinovic@gmail.com>
2022-11-01 12:23:30 +0100

    create routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,26 @@
 import React from 'react'
-import './App.scss'
 import { Container } from 'react-bootstrap'
-import Register from './pages/Register'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import ApolloProvider from './ApolloProvider'
+
+import './App.scss'
+
+import Home from './pages/Home'
+import Register from './pages/Register'
+import Login from './pages/Login'
 
 function App() {
   return (
     <ApolloProvider>
-      <Container className='pt-5'>
-        <Register/>
-      </Container>
+      <BrowserRouter>
+        <Container className='pt-5'>
+          <Routes>
+            <Route exact path='/' element={<Home/>} />
+            <Route path='/register' element={<Register/>} />
+            <Route path='/login' element={<Login/>} />
+          </Routes>
+        </Container>
+      </BrowserRouter>
     </ApolloProvider>
   )
 }
